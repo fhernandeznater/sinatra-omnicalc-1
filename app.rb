af4ffb6f8dc4224869aea2a5ccd01d22d06d39ab
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-03-01 00:32:34 +0000

    Added RNG

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ get("/howdy") do
   erb(:hello)
 end
 
+get("/") do
+  erb(:square)
+end
+
 get("/goodbye") do
   erb(:bye)
 end
@@ -31,8 +35,21 @@ get("/square_root/new") do
   erb(:sroot)
 end
 
+get("/square_root/results") do
+  @num_to_root = params.fetch("root_number").to_f
+  @root_result = @num_to_root ** 0.5
+  erb(:sroot_results)
+end
+
 get("/random/new") do
   erb(:rng)
+end
+
+get("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+  @random_number = rand * (@max - @min) + @min
+  erb(:rng_results)
 end
 
 get("payment/new") do
